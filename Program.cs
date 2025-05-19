75026ca3455fb9ae9b2d909cd34d34c38be47135
SpaceCattStalin <spacestalincyka@gmail.com>
2025-05-19 20:19:40 +0700

    Add Swagger and Docker

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
-using Yarp.ReverseProxy;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("http://localhost:5003/swagger/v1/swagger.json", "Knowledgebase API");
+        c.SwaggerEndpoint("http://localhost:5001/swagger/v1/swagger.json", "Chat API");
+        c.SwaggerEndpoint("http://localhost:5002/swagger/v1/swagger.json", "User API");
+    });
+}
+
 
 app.MapReverseProxy();
 
