6892d0a1bc929e02c2cc6000acd76ab8443d7c59
SpaceCattStalin <spacestalincyka@gmail.com>
2025-06-19 14:09:53 +0700

    Add Application API

--- Program.cs
+++ Program.cs
@@ -1,22 +1,47 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowGateway", policy =>
+    {
+        policy.WithOrigins("*")
+         .AllowAnyHeader()
+         .AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
-//if (app.Environment.IsDevelopment())
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/user/swagger/v1/swagger.json", "User API");
-    c.SwaggerEndpoint("/lead/swagger/v1/swagger.json", "Lead API");
-    c.SwaggerEndpoint("/collegeinfo/swagger/v1/swagger.json", "College Info API");
-
-});
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("http://localhost:5002/swagger/v1/swagger.json", "User API");
+        c.SwaggerEndpoint("http://localhost:5012/swagger/v1/swagger.json", "Lead API");
+        c.SwaggerEndpoint("http://localhost:5010/swagger/v1/swagger.json", "College Info API");
+        c.SwaggerEndpoint("http://localhost:5014/swagger/v1/swagger.json", "Application API");
+    });
+}
+else
+{
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/user/v1/swagger.json", "User API");
+        c.SwaggerEndpoint("/swagger/lead/v1/swagger.json", "Lead API");
+        c.SwaggerEndpoint("/swagger/collegeinfo/v1/swagger.json", "College Info API");
+    });
+}
+app.UseCors("AllowGateway");
 
 app.MapReverseProxy();
 
